962354c9d2b1c13511ff84e522aa025afc9b7113
Aman Verma <amanraoverma@gmail.com>
2020-12-04 15:51:26 -0500

    Fixes for macOS.

--- source/getentropy_getentropy_macos.c
+++ source/getentropy_getentropy_macos.c
@@ -4,17 +4,18 @@
 // clang-format off
 #include "xp_getentropy.h"
 // sys/random.h is not self-contained on macOS. It needs Availibility.h before it.
-#include <Availibility.h>
+#include <Availability.h>
 #include <sys/random.h>
 #include <errno.h>
 // clang-format on
 
 int xp_getentropy(void *buf, size_t len) {
     if (getentropy(buf, len) == -1) {
-        // macOS sets errno to EINVAL for invalid address. Standard OpenBSD behavior is
-        // EFAULT.
+        // macOS sets errno to EINVAL if len > 256, contrary to the man page.
+        // See https://opensource.apple.com/source/xnu/xnu-6153.141.1/bsd/dev/random/randomdev.c.auto.html.
+        // Rectify to conform to OpenBSD behavior.
         if (errno == EINVAL) {
-            errno = EFAULT;
+            errno = EIO;
         }
         return -1;
     }
