9afff64edfa206eebdde8672546fb44753a5b727
Aman Verma <amanraoverma@gmail.com>
2020-12-06 22:49:17 -0500

    Remove Doxygen as it is unnecessary for such a simple library.

--- include/xp_getentropy.h
+++ include/xp_getentropy.h
@@ -6,12 +6,11 @@
 
 #include <stddef.h>
 
-/// @brief Fill a buffer with high-quality entropy. The maximum permitted buffer size is
+/// Fill a buffer with high-quality entropy. The maximum permitted buffer size is
 /// 256 bytes.
 /// @param[out] buf The buffer to fill.
 /// @param[in] len The length of the buffer, in bytes.
-/// @retval 0 Succeeded.
-/// @retval -1 Failed.
+/// @return 0 on success and -1 on failure.
 int xp_getentropy(void *buf, size_t len);
 
 #endif
