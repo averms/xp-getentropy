c1cddb419116a06577126d8c5f63c751b3e2a81a
Aman Verma <amanraoverma@gmail.com>
2020-12-11 02:47:16 -0500

    Switch Windows backend to use RtlGenRandom.
    It makes for smaller binaries and makes the library easier to build.

--- source/getentropy_rtlgenrandom.c
+++ source/getentropy_rtlgenrandom.c
@@ -19,25 +19,28 @@
 
 #include "xp_getentropy.h"
 #include <sys/types.h>
-#include <bcrypt.h>
 #include <errno.h>
 #include <stdint.h>
 #include <windows.h>
 
-#pragma comment(lib, "bcrypt.lib")
-
 /*
- * On Windows, BCryptGenRandom with BCRYPT_USE_SYSTEM_PREFERRED_RNG is supposed
- * to be a well-seeded, cryptographically strong random number generator.
- * https://docs.microsoft.com/en-us/windows/win32/api/bcrypt/nf-bcrypt-bcryptgenrandom
+ * On Windows, RtlGenRandom is supposed to output cryptographically-secure random bytes.
+ * Despite the warning in the docs, it will probably not be removed:
+ * https://github.com/rust-random/rand/issues/111#issuecomment-316140155.
+ *
+ * https://docs.microsoft.com/en-us/windows/win32/api/ntsecapi/nf-ntsecapi-rtlgenrandom
+ * https://www.microsoft.com/security/blog/2019/11/25/going-in-depth-on-the-windows-10-random-number-generation-infrastructure/
  */
+#define RtlGenRandom SystemFunction036
+BOOLEAN NTAPI RtlGenRandom(PVOID RandomBuffer, ULONG RandomBufferLength);
+
 int xp_getentropy(void* buf, size_t len) {
     if (len > 256) {
         errno = EIO;
         return -1;
     }
 
-    if (FAILED(BCryptGenRandom(NULL, buf, len, BCRYPT_USE_SYSTEM_PREFERRED_RNG))) {
+    if (!RtlGenRandom((PVOID)buf, (ULONG)len)) {
         errno = EIO;
         return -1;
     }
